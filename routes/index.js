f371063f45ca9de8d4482e28d01f0937be7a2ccb
orozco92 <goldstein.david92@gmail.com>
2022-04-19 18:38:47 -0500

    update route order

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const mainRouter = require('./main');
 const usersRouter = require('./users');
 
 function init(app) {
-  app.use(prefix, mainRouter);
   app.use(prefix + '/users', usersRouter);
+  app.use(prefix, mainRouter);
 }
 
 module.exports.init = init;
